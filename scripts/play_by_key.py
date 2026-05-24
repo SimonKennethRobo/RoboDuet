03f4036bb8b512d2d2d40e3dd548fb54efa1d913
longbintang <longbin@u.nus.edu>
2026-05-24 13:18:35 +0800

    Fix stage1 keyboard play controls

--- scripts/play_by_key.py
+++ scripts/play_by_key.py
@@ -3,7 +3,8 @@ import isaacgym
 import torch
 from isaacgym.torch_utils import *
 from go1_gym.envs import *
-from go1_gym.envs.roboduet import KeyboardWrapper
+from go1_gym.envs.roboduet.wbc_env_wrapper import KeyboardWrapper
+from go1_gym.envs.roboduet.wbc_env_config import configure_privileged_obs_dims
 from scripts.load_policy import load_dog_policy, load_arm_policy, load_env
 import argparse
 
@@ -22,15 +23,32 @@ def play_go1(args):
     ckpt_id = str(args.ckptid).zfill(6)
 
     from go1_gym.utils.global_switch import global_switch
-    global_switch.open_switch()
 
-    env, cfg = load_env(logdir, wrapper=KeyboardWrapper, headless=args.headless, device=args.sim_device)
+    stage1_only = bool(getattr(args, "stage1_only", False))
+    if stage1_only:
+        global_switch.switch_flag = False
+        global_switch.count = 0
+        global_switch.stage1_arm_ramp_iterations = 1
+        global_switch.stage1_count = 1
+        global_switch.pretrained_to_hybrid_start = args.num_eval_steps + 1
+        global_switch.pretrained_to_hybrid_end = global_switch.pretrained_to_hybrid_start + 1
+    else:
+        global_switch.open_switch()
+
+    env, cfg = load_env(
+        logdir,
+        wrapper=KeyboardWrapper,
+        headless=args.headless,
+        device=args.sim_device,
+        robot=getattr(args, "robot", None),
+    )
     dog_policy = load_dog_policy(logdir, ckpt_id, cfg)
-    arm_policy = load_arm_policy(logdir, ckpt_id, cfg)
+    arm_policy = None if stage1_only else load_arm_policy(logdir, ckpt_id, cfg)
+    configure_privileged_obs_dims(cfg)
 
     env.env.enable_viewer_sync = True
 
-    num_eval_steps = 30000
+    num_eval_steps = args.num_eval_steps
 
     ''' press 'F' to fixed camera'''
     # cam_pos = gymapi.Vec3(4, 3, 2)
@@ -48,22 +66,25 @@ def play_go1(args):
     env.commands_arm[:, 3] = roll_cmd
     env.commands_arm[:, 4] = pitch_cmd
     env.commands_arm[:, 5] = yaw_cmd
+    if hasattr(env.env, "_reset_keyboard_play_commands"):
+        env.env._reset_keyboard_play_commands()
 
-    count = 0
-
-    obs = env.get_arm_observations()
     for i in (range(num_eval_steps)):
 
         with torch.no_grad():
-            t1 = time.time()
-
-            obs = env.get_arm_observations()
-            actions_arm = arm_policy(obs)
-            env.plan(actions_arm[..., -2:])
+            if arm_policy is None:
+                actions_arm = env.arm_fake_actions
+            else:
+                obs = env.get_arm_observations()
+                actions_arm = arm_policy(obs).to(env.env.device)
+                env.plan(actions_arm[..., -2:])
 
             dog_obs = env.get_dog_observations()
-            actions_dog = dog_policy(dog_obs)
-        ret = env.step(actions_dog, actions_arm[...,:-2], )
+            actions_dog = dog_policy(dog_obs).to(env.env.device)
+        if arm_policy is None:
+            ret = env.step(actions_dog, actions_arm)
+        else:
+            ret = env.step(actions_dog, actions_arm[..., :-2])
 
 
 
@@ -74,7 +95,14 @@ if __name__ == '__main__':
     parser.add_argument('--sim_device', type=str, default="cuda:0")
     parser.add_argument('--logdir', type=str)
     parser.add_argument('--ckptid', type=int, default=40000)
+    parser.add_argument('--robot', type=str, default="go2", choices=["go1", "go2"])
+    parser.add_argument('--num_eval_steps', type=int, default=30000)
+    parser.add_argument(
+        '--stage1_only',
+        action='store_true',
+        default=False,
+        help='Run pure stage1 play: keep global switch closed and do not load/call the arm policy.',
+    )
 
     args = parser.parse_args()
     play_go1(args)
-
